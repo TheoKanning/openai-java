4f938720f7e91831ccb5a94efff2efb4296676c4
Theo Kanning <theokanning@gmail.com>
2022-04-28 14:42:14 -0500

    Add more tests and run them before each merge
    These tests are very basic sanity checks

---
+++ client/src/test/java/com/theokanning/openai/CompletionTest.java
@@ -0,0 +1,27 @@
+package com.theokanning.openai;
+
+import com.theokanning.openai.completion.CompletionChoice;
+import com.theokanning.openai.completion.CompletionRequest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+public class CompletionTest {
+
+    String token = System.getenv("OPENAI_TOKEN");
+    OpenAiService service = new OpenAiService(token);
+
+    @Test
+    void createCompletion() {
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .prompt("Somebody once told me the world is gonna roll me")
+                .echo(true)
+                .build();
+
+        List<CompletionChoice> choices = service.createCompletion("ada", completionRequest).getChoices();
+        assertFalse(choices.isEmpty());
+    }
+}
