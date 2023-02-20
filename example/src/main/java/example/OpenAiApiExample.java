783c72d438a602b5f53bd1231f124fc9d01cb2f0
Vatsa <9.vatsa@gmail.com>
2023-02-20 02:14:05 -0500

    fix(src): modify the OpenAiApiExample
    - Add examples for multiple implementations of OpenAiService

--- example/src/main/java/example/OpenAiApiExample.java
+++ example/src/main/java/example/OpenAiApiExample.java
@@ -5,9 +5,14 @@ import com.theokanning.openai.completion.CompletionRequest;
 
 class OpenAiApiExample {
     public static void main(String... args) {
+        OpenAiApiExample openAiApiExample = new OpenAiApiExample();
+        openAiApiExample.OpenAiServiceWithOnlyToken();
+        openAiApiExample.OpenAiServiceWithTokenAndUrlAndTimeOut();
+    }
+
+    public void OpenAiServiceWithOnlyToken(){
         String token = System.getenv("OPENAI_TOKEN");
         OpenAiService service = new OpenAiService(token);
-
         System.out.println("\nCreating completion...");
         CompletionRequest completionRequest = CompletionRequest.builder()
                 .model("ada")
@@ -17,4 +22,19 @@ class OpenAiApiExample {
                 .build();
         service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
     }
+
+    public void OpenAiServiceWithTokenAndUrlAndTimeOut(){
+        String token = System.getenv("OPENAI_TOKEN");
+        final String BASE_URL = "https://api.openai.com/";
+        OpenAiService service = new OpenAiService(token, BASE_URL,  Duration.ofSeconds(10));
+        System.out.println("\nCreating completion...");
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .model("ada")
+                .prompt("Somebody once told me the world is gonna roll me")
+                .echo(true)
+                .user("testing")
+                .build();
+        service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
+    }
+
 }
