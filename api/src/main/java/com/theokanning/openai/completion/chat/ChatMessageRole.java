76a849b5904d2c763fb1f7cf7de9e191cf45d683
Ke30 <email@email.com>
2023-03-01 22:24:23 +0100

    fix: switch enum to string enum

--- api/src/main/java/com/theokanning/openai/completion/chat/ChatMessageRole.java
+++ api/src/main/java/com/theokanning/openai/completion/chat/ChatMessageRole.java
@@ -4,7 +4,17 @@ package com.theokanning.openai.completion.chat;
  * see {@link ChatMessage} documentation.
  */
 public enum ChatMessageRole {
-    system,
-    user,
-    assistant
+    SYSTEM("system"),
+    USER("user"),
+    ASSISTANT("assistant");
+
+    private final String value;
+
+    ChatMessageRole(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
