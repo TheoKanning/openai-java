05b949322af03f7ae4b7be19c1ec5129b2176695
mzhu-ai <mengran.zhu0504@gmail.com>
2023-03-03 22:13:54 -0800

    add no arg constructor to ChatCompletionRequest

--- api/src/main/java/com/theokanning/openai/completion/chat/ChatCompletionRequest.java
+++ api/src/main/java/com/theokanning/openai/completion/chat/ChatCompletionRequest.java
@@ -1,13 +1,17 @@
 package com.theokanning.openai.completion.chat;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatCompletionRequest {
 
     /**
