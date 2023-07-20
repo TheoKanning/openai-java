4aa536f8453b4d8e2de126c1822f81d3d168d66e
Sardor Taylakov <staylakov@tripactions.com>
2023-07-20 13:27:30 -0700

    use-non-deprecated-jackson-enum
    use PropertyNamingStrategies instead of PropertyNamingStrategy

--- service/src/main/java/com/theokanning/openai/service/OpenAiService.java
+++ service/src/main/java/com/theokanning/openai/service/OpenAiService.java
@@ -3,7 +3,7 @@ package com.theokanning.openai.service;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.node.TextNode;
 import com.theokanning.openai.DeleteResult;
 import com.theokanning.openai.OpenAiError;
@@ -385,7 +385,7 @@ public class OpenAiService {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         mapper.addMixIn(ChatFunction.class, ChatFunctionMixIn.class);
         mapper.addMixIn(ChatCompletionRequest.class, ChatCompletionRequestMixIn.class);
         mapper.addMixIn(ChatFunctionCall.class, ChatFunctionCallMixIn.class);
