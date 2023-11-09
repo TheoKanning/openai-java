23f9ef6b78949d3cd54843fbc4c4a2e57b9076e5
vacuity <vacuityv@icloud.com>
2023-11-09 11:38:09 +0800

    feat(image): new feature for dalle api.
    You can set model, quality and style now.

--- api/src/main/java/com/theokanning/openai/image/CreateImageRequest.java
+++ api/src/main/java/com/theokanning/openai/image/CreateImageRequest.java
@@ -17,18 +17,28 @@ import lombok.*;
 public class CreateImageRequest {
 
     /**
-     * A text description of the desired image(s). The maximum length in 1000 characters.
+     * A text description of the desired image(s). The maximum length is 1000 characters for dall-e-2 and 4000 characters for dall-e-3.
      */
     @NonNull
     String prompt;
 
     /**
-     * The number of images to generate. Must be between 1 and 10. Defaults to 1.
+     * The model to use for image generation. Defaults to "dall-e-2".
+     */
+    String model;
+
+    /**
+     * The number of images to generate. Must be between 1 and 10. For dall-e-3, only n=1 is supported. Defaults to 1.
      */
     Integer n;
 
     /**
-     * The size of the generated images. Must be one of "256x256", "512x512", or "1024x1024". Defaults to "1024x1024".
+     * The quality of the image that will be generated. "hd" creates images with finer details and greater consistency across the image. This param is only supported for dall-e-3. Defaults to "standard".
+     */
+    String quality;
+
+    /**
+     * The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024 for dall-e-2. Must be one of 1024x1024, 1792x1024, or 1024x1792 for dall-e-3 models. Defaults to 1024x1024.
      */
     String size;
 
@@ -37,6 +47,11 @@ public class CreateImageRequest {
      */
     @JsonProperty("response_format")
     String responseFormat;
+
+    /**
+     * The style of the generated images. Must be one of vivid or natural. Vivid causes the model to lean towards generating hyper-real and dramatic images. Natural causes the model to produce more natural, less hyper-real looking images. This param is only supported for dall-e-3. Defaults to vivid.
+     */
+    String style;
 
     /**
      * A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse.
