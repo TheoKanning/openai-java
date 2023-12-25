ceb460b81f39fbf5b956ffb21b586e736617917a
barak <bn.barak@gmail.com>
2023-12-24 22:06:37 -0500

    Refine the messages print.

--- example/src/main/java/example/OpenAiAssistantExample.java
+++ example/src/main/java/example/OpenAiAssistantExample.java
@@ -4,6 +4,7 @@ import com.theokanning.openai.OpenAiResponse;
 import com.theokanning.openai.assistants.Assistant;
 import com.theokanning.openai.assistants.AssistantRequest;
 import com.theokanning.openai.messages.Message;
+import com.theokanning.openai.messages.MessageContent;
 import com.theokanning.openai.messages.MessageRequest;
 import com.theokanning.openai.runs.CreateThreadAndRunRequest;
 import com.theokanning.openai.runs.Run;
@@ -19,20 +20,27 @@ final class OpenAiAssistantExample {
     Run run;
     do {
       run = service.retrieveRun(threadId, runId);
-      // Or do something better to wait for the status to change.
+      // Or do something better to wait for the status to change. As for EOY 2023 it takes a few
+      // seconds.
       TimeUnit.SECONDS.sleep(1);
     } while (!Objects.equals(run.getStatus(), "completed"));
   }
 
-  private static void print(OpenAiResponse<Message> messageOpenAiResponse) {
-    for (Message message : messageOpenAiResponse.getData()) {
-      System.out.printf("[%s] %s%n", message.getRole(), message.getContent().get(0));
+  private static void print(OpenAiResponse<Message> messages) {
+    for (Message message : messages.getData()) {
+      StringBuilder sb = new StringBuilder();
+      for(MessageContent content : message.getContent()) {
+        sb.append(content);
+      }
+
+      System.out.printf("[%s] %s%n", message.getRole(), sb);
     }
   }
 
   public static void main(String... args) throws InterruptedException {
     String token = System.getenv("OPENAI_TOKEN");
     OpenAiService service = new OpenAiService(token, Duration.ofSeconds(30));
+
     System.out.println("\nCreate a new assistant....");
     Assistant assistant =
         service.createAssistant(
@@ -48,32 +56,33 @@ final class OpenAiAssistantExample {
             + " assistant...");
     CreateThreadAndRunRequest createThreadAndRunRequest =
         CreateThreadAndRunRequest.builder().assistantId(assistantId).build();
-
     Run run = service.createThreadAndRun(createThreadAndRunRequest);
+
     System.out.println(
         "\nKeep the thread id which is a persistent id for the conversation between the user and the\n"
             + " assistant...");
     String threadId = run.getThreadId();
 
-    System.out.println("\nWait for gpt to process the new thread...");
+    System.out.println("\nWait for GPT to process the new thread...");
     waitForRunToComplete(service, run.getId(), threadId);
 
-    System.out.println("\nGet the latest messages list, the one message should be from the assistant...");
-    OpenAiResponse<Message> messageOpenAiResponse = service.listMessages(threadId);
-    print(messageOpenAiResponse);
+    System.out.println(
+        "\nGet the latest messages list, should contain a single message from the assistant...");
+    OpenAiResponse<Message> messages = service.listMessages(threadId);
+    print(messages);
 
-    System.out.println("\nAdd a new message to the thread...");
+    System.out.println("\nAdd a new user message to the thread...");
     service.createMessage(
         threadId, MessageRequest.builder().content("Can you solve 1+1?").role("user").build());
 
-    System.out.println("\nCreate a new run for gpt to response to the message...");
+    System.out.println("\nCreate a new run for GPT to response to the message...");
     run = service.createRun(threadId, RunCreateRequest.builder().assistantId(assistantId).build());
 
     System.out.println("\nWait again to the run to complete...");
     waitForRunToComplete(service, run.getId(), threadId);
 
     System.out.println("\nObserve the latest messages after the second run...");
-    messageOpenAiResponse = service.listMessages(threadId);
-    print(messageOpenAiResponse);
+    messages = service.listMessages(threadId);
+    print(messages);
   }
 }
